965c076eaffb9875ef8ab456e5619aa7850e7f15
maxnazaruk <max.nazaruk@gmail.com>
2017-07-18 19:48:12 +0200

    Add files via upload

---
+++ fifteenth/src/mainPack/secondary.java
@@ -0,0 +1,13 @@
+package mainPack;
+
+/**
+ * Created by Max on 7/18/2017.
+ */
+public class secondary {
+    public static void main(String[] args) {
+        int[][] arr = {{0, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 1}};
+    }
+}
